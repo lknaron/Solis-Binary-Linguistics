ea79bda3224adb2fd72f8542f12ee59a8e3f1b0f
Larry Naron <lknaron@asu.edu>
2017-02-01 21:10:52 -0600

    adds routing and basic controllers to application pages

--- client/app/application/application.js
+++ client/app/application/application.js
@@ -1,5 +1,103 @@
 /*
- * application.js handles the application process and routing
- * 
- * So app.js module ('app') will include this module ('application'). 
+ * File: application.js
+ * Description: Module and controllers to handle pages of the TA/Grader application process.
  */
+
+// setup module
+var application = angular.module('app.application', []);
+
+application.controller('contactInfoController', function($scope, $location, $http) {
+    $scope.saveContact = function(doRoute) {
+        /*
+        $http.post(route, data).then(function successCallback(response) {
+            console.log('successful post');
+            }            
+        }, function errorCallback(response) {
+            console.log('unsuccessful post');
+            //TODO
+        });*/
+        if (doRoute === true) {
+           $location.path('/education'); 
+        }
+    }
+});
+
+application.controller('educationInfoController', function($scope, $location, $http) {
+    $scope.saveEducation = function(doRoute) {
+        /*
+        $http.post(route, data).then(function successCallback(response) {
+            console.log('successful post');
+            }            
+        }, function errorCallback(response) {
+            console.log('unsuccessful post');
+            //TODO
+        });*/
+        if (doRoute === true) {
+           $location.path('/employment'); 
+        }
+    }
+});
+
+application.controller('employmentInfoController', function($scope, $location, $http) {
+    $scope.saveEmployment = function(doRoute) {
+        /*
+        $http.post(route, data).then(function successCallback(response) {
+            console.log('successful post');
+            }            
+        }, function errorCallback(response) {
+            console.log('unsuccessful post');
+            //TODO
+        });*/
+        if (doRoute === true) {
+           $location.path('/availability'); 
+        }
+    }
+});
+
+application.controller('availabilityInfoController', function($scope, $location, $http) {
+    $scope.saveAvailability = function(doRoute) {
+        /*
+        $http.post(route, data).then(function successCallback(response) {
+            console.log('successful post');
+            }            
+        }, function errorCallback(response) {
+            console.log('unsuccessful post');
+            //TODO
+        });*/
+        if (doRoute === true) {
+           $location.path('/languages'); 
+        }
+    }
+});
+
+application.controller('languagesInfoController', function($scope, $location, $http) {
+    $scope.saveLanguages= function(doRoute) {
+        /*
+        $http.post(route, data).then(function successCallback(response) {
+            console.log('successful post');
+            }            
+        }, function errorCallback(response) {
+            console.log('unsuccessful post');
+            //TODO
+        });*/
+        if (doRoute === true) {
+           $location.path('/courses'); 
+        }
+    }
+});
+
+application.controller('coursesInfoController', function($scope, $location, $http) {
+    $scope.saveCourses= function(doRoute) {
+        /*
+        $http.post(route, data).then(function successCallback(response) {
+            console.log('successful post');
+            }            
+        }, function errorCallback(response) {
+            console.log('unsuccessful post');
+            //TODO
+        });*/
+        if (doRoute === true) {
+           //TODO; 
+        }
+    }
+});
