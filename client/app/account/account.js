99471db1848aedeee7d41ff1c6121eb49aeaa984
Larry Naron <lknaron@asu.edu>
2017-02-03 17:30:19 -0600

    added field validation to createAccount and password match check to the createAccountController

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -9,38 +9,42 @@ var account = angular.module('app.account', ['ngRoute']);
 
 // account pages controllers
 account.controller('createAccountController', function($scope, $location, $http, UserInfoService) {
-    // Larry:  Need to ensure first and second password entries match
     var regTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    $scope.submitAccount = function() {
-        $scope.message = ''; 
-        
-        var createAccountData = {
-           "ASURITE_ID" : $scope.asuriteID,
-            "FirstName"  : $scope.firstname,
-            "MiddleName"  : $scope.middlename,
-            "LastName"  : $scope.lastname,
-            "UserEmail"  : $scope.email,
-            "UserPassword"  : $scope.password1,
-            "UserRole"  : "student",
-            "RegTime"  : regTime,
-            "isActive"  : 1,
-            "LoginTime" : regTime
-        };
-
-        $http.post('/createAccount', createAccountData).then(function successCallback(response) {
-            if (response.data.error === 0) {
-                UserInfoService.setUserId($scope.username);
-                UserInfoService.setFullName($scope.firstname + ' ' + $scope.lastname);
-                UserInfoService.setUserType('student'); 
-                $location.path('/studentHome');               
-            } else if(response.data.error === 1) {
-                console.log(response.data.error);
-                $scope.message = 'User already exists';
-            }        
-        }, function errorCallback(response) {
-            console.log('unsuccessful post');
-            //TODO
-        });
+    $scope.submitAccount = function() {     
+        $scope.message = '';        
+        // checks if passwords match, then posts if match
+        if ($scope.password1 !== $scope.password2) {
+            $scope.message = 'Passwords do not match. Please try again.';
+            $scope.password1 = '';
+            $scope.password2 = '';
+        } else {
+            var createAccountData = {
+               "ASURITE_ID" : $scope.asuriteID,
+                "FirstName"  : $scope.firstname,
+                "MiddleName"  : $scope.middlename,
+                "LastName"  : $scope.lastname,
+                "UserEmail"  : $scope.email,
+                "UserPassword"  : $scope.password1,
+                "UserRole"  : "student",
+                "RegTime"  : regTime,
+                "isActive"  : 1,
+                "LoginTime" : regTime
+            };
+            $http.post('/createAccount', createAccountData).then(function successCallback(response) {
+                if (response.data.error === 0) {
+                    UserInfoService.setUserId($scope.username);
+                    UserInfoService.setFullName($scope.firstname + ' ' + $scope.lastname);
+                    UserInfoService.setUserType('student'); 
+                    $location.path('/studentHome');               
+                } else if(response.data.error === 1) {
+                    console.log(response.data.error);
+                    $scope.message = 'User already exists';
+                }        
+            }, function errorCallback(response) {
+                console.log('unsuccessful post');
+                //TODO
+            });
+        }
     }
 });
 
