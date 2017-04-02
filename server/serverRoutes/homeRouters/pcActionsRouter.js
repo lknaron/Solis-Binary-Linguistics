fde85387b80bcfd672aea9f28f39ca9004a6ccaa
Larry Naron <lknaron@asu.edu>
2017-04-01 21:30:52 -0500

    added course placement checks

--- server/serverRoutes/homeRouters/pcActionsRouter.js
+++ server/serverRoutes/homeRouters/pcActionsRouter.js
@@ -41,19 +41,85 @@ router.post('/', function(req, res) {
                 if (rows.length > 0) {
                     hasActions = 1;
                 }
+                var placements = {};
+                placements = courseTAandGraderChecks(placementData);
                 connection.query("SELECT DateCreated FROM Application WHERE DateCreated > ? AND AppStatus != 'new'", [req.body.lastLogin], function(err3, newAppsCount) {                 
                     connection.query("SELECT AppStatus FROM Application WHERE AppStatus = 'incomplete'", function(err4, incompleteCount) {
                         connection.query("SELECT AppStatus FROM Application WHERE AppStatus = 'complete'", function(err5, completeCount) {
                             connection.query("SELECT DeadlineDate FROM Deadline", function(err5, deadlineDate) {
-                               res.send({hasActions:hasActions, incompleteClasses:incompleteClasses, placements:placementData, newApps:newAppsCount.length, incompleteApps:incompleteCount.length, completeApps:completeCount.length, deadline:deadlineDate[0].DeadlineDate}); 
+                               res.send({hasActions:hasActions, incompleteClasses:incompleteClasses, placements:placements, newApps:newAppsCount.length, incompleteApps:incompleteCount.length, completeApps:completeCount.length, deadline:deadlineDate[0].DeadlineDate}); 
                             });
                         });
                     });
                 });
             });
-        });
-        
+        });        
         connection.release();
     });
+    function courseTAandGraderChecks(data) {
+        var coursesMissingTA = [];
+        var coursesMissingGrader = [];
+        var coursesNeedTAHours = [];
+        var coursesNeedGraderHours = [];
+        var coursesNeedTAConfirmation = [];
+        var coursesNeedGraderConfirmation = [];
+        var firstTAHours = 0;
+        var secondTAHours = 0;
+        var firstGraderHours = 0;
+        var secondGraderHours = 0;
+        for (var i = 0; i < data.length; i++) {
+            // checks if each course is missing a TA assignment
+            if (data[i].TA === null && data[i].TATwo === null) {
+                coursesMissingTA.push({Location:data[i].Location, Subject:data[i].Subject, CatalogNumber:data[i].CatalogNumber, CourseNumber:data[i].CourseNumber});
+            }
+            // checks if each course is missing a Grader assignment
+            if (data[i].GraderOne === null && data[i].GraderTwo === null) {
+                coursesMissingGrader.push({Location:data[i].Location, Subject:data[i].Subject, CatalogNumber:data[i].CatalogNumber, CourseNumber:data[i].CourseNumber});
+            }
+            // checks if each course is not meeting the required TA hours
+            if (data[i].TARequiredHours !== null) {
+                if (data[i].TAHours !== null) {
+                    firstTAHours = data[i].TAHours;
+                } 
+                if (data[i].TATwoHours !== null) {
+                    secondTAHours = data[i].TATwoHours;
+                }
+                var assignedTAHours = firstTAHours + secondTAHours;
+                if (assignedTAHours < data[i].TARequiredHours) {
+                    var missingTAHours = data[i].TARequiredHours - assignedTAHours;
+                    coursesNeedTAHours.push({Location:data[i].Location, Subject:data[i].Subject, CatalogNumber:data[i].CatalogNumber, CourseNumber:data[i].CourseNumber, neededHours:missingTAHours});
+                }
+            }
+            // checks if each course is not meeting the required Grader hours
+            if (data[i].GraderRequiredHours !== null) {
+                if (data[i].GraderOneHours !== null) {
+                    firstGraderHours = data[i].GraderOneHours;
+                } 
+                if (data[i].GraderTwoHours !== null) {
+                    secondGraderHours = data[i].GraderTwoHours;
+                }
+                var assignedGraderHours = firstGraderHours + secondGraderHours;
+                if (assignedGraderHours < data[i].GraderRequiredHours) {
+                    var missingGraderHours = data[i].GraderRequiredHours - assignedGraderHours;
+                    coursesNeedGraderHours.push({Location:data[i].Location, Subject:data[i].Subject, CatalogNumber:data[i].CatalogNumber, CourseNumber:data[i].CourseNumber, neededHours:missingGraderHours});
+                }
+            }
+            // checks if each course has a TA not confirmed            
+            if ((data[i].TA !== null && data[i].TAStatus !== 'Confirmed') || (data[i].TATwo !== null && data[i].TATwoStatus !== 'Confirmed')) {
+                coursesNeedTAConfirmation.push({Location:data[i].Location, Subject:data[i].Subject, CatalogNumber:data[i].CatalogNumber, CourseNumber:data[i].CourseNumber});
+            }
+            // checks if each course has a Grader not confirmed            
+            if ((data[i].GraderOne !== null && data[i].GraderOneStatus !== 'Confirmed') || (data[i].GraderTwo !== null && data[i].GraderTwoStatus !== 'Confirmed')) {
+                coursesNeedGraderConfirmation.push({Location:data[i].Location, Subject:data[i].Subject, CatalogNumber:data[i].CatalogNumber, CourseNumber:data[i].CourseNumber});
+            }
+        }
+        //console.log(coursesMissingTA);
+        //console.log(coursesMissingGrader);
+        //console.log(coursesNeedTAHours);
+        //console.log(coursesNeedGraderHours);
+        //console.log(coursesNeedTAConfirmation);
+        //console.log(coursesNeedGraderConfirmation);
+        return {missingTA:coursesMissingTA, missingGrader:coursesMissingGrader, needTAHours:coursesNeedTAHours, needGraderHours:coursesNeedGraderHours, needTAConfirmation:coursesNeedTAConfirmation, needGraderConfirmation:coursesNeedGraderConfirmation};
+    }
 });   
 module.exports = router;
